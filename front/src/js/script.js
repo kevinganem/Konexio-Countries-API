d3a5203bb5257ddc6f66ced7cc1df319eaae196d
Kevin GANEM <Ganemkevin@gmail.com>
2022-01-19 15:32:37 +0100

    fixing html / js

---
+++ front/src/js/script.js
@@ -0,0 +1,24 @@
+$("#btnShowData").click(function () {
+    let country = $("input").val();
+    $.ajax({
+        url: `https://restcountries.com/v3.1/name/${country}`,
+        success: function (data) {
+            data.forEach((country) => {
+                    $("#country").html(`<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`);
+            })
+        },
+    });
+});
+
+function getAllCountries() {
+    $.ajax({
+        url: "https://restcountries.com/v3.1/all",
+        success: function (data) {
+            data.forEach((country) => {
+                $("#country").html(`<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`)
+            })
+        }
+    })
+}
+
+getAllCountries();
