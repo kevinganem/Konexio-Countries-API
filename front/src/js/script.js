459fa9f20a18c0b0bedba0d27e3a828d4bbdf9db
Kevin GANEM <Ganemkevin@gmail.com>
2022-01-20 12:23:29 +0100

    load button

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -5,10 +5,14 @@ $("#btnShowData").click(function () {
   let search = $("input[id=search]:checked", "#searchPart").val();
 
   if (country) {
+    $("#btnShowData").addClass("visually-hidden");
+    $("#btnLoading").removeClass("visually-hidden");
     if (search === "country" || !search) {
       $.ajax({
         url: `https://restcountries.com/v3.1/name/${country}`,
         success: function (data) {
+          $("#btnShowData").removeClass("visually-hidden");
+          $("#btnLoading").addClass("visually-hidden");
           data.forEach((country) => {
             $("#country").html(
               `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
@@ -20,6 +24,8 @@ $("#btnShowData").click(function () {
       $.ajax({
         url: `https://restcountries.com/v3.1/capital/${country}`,
         success: function (data) {
+          $("#btnShowData").removeClass("visually-hidden");
+          $("#btnLoading").addClass("visually-hidden");
           data.forEach((country) => {
             $("#country").html(
               `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
@@ -32,6 +38,14 @@ $("#btnShowData").click(function () {
     getAllCountries();
   }
 });
+
+// RESET FUNCTION
+
+$("#btnReset").click(function () {
+  getAllCountries();
+});
+
+// SELECT FUNCTION
 
 $("select").change(function () {
   select = $("select option:selected").val();
@@ -54,9 +68,14 @@ $("select").change(function () {
 // API REQUEST + LIST ALL
 
 function getAllCountries() {
+  $("#btnShowData").addClass("visually-hidden");
+  $("#btnLoading").removeClass("visually-hidden");
   $.ajax({
     url: "https://restcountries.com/v3.1/all",
     success: function (data) {
+      $("#btnShowData").removeClass("visually-hidden");
+      $("#btnLoading").addClass("visually-hidden");
+      $("li").remove();
       data.forEach((country) => {
         $("#country").append(
           `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
