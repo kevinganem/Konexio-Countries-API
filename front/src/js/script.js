8b63dd133d942eb2609eeb8d5522bdd592f7b170
Kevin GANEM <Ganemkevin@gmail.com>
2022-01-20 11:05:37 +0100

    trying to fix issues for the select part

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -1,49 +1,68 @@
 // BUTTON FUNCTION
 
 $("#btnShowData").click(function () {
-    let country = $("input").val();
-    let search = $('input[id=search]:checked', '#searchPart').val()
+  let country = $("input").val();
+  let search = $("input[id=search]:checked", "#searchPart").val();
 
-    console.log(country);
-    console.log(search);
-    console.log(country);
-    if (country) {
-        if (search === 1 || !search) {
-            console.log(country);
-            $.ajax({
-                url: `https://restcountries.com/v3.1/name/${country}`,
-                success: function (data) {
-                    data.forEach((country) => {
-                            $("#country").html(`<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`);
-                    })
-                },
-            });
-        } else {
-            $.ajax({
-                url: `https://restcountries.com/v3.1/capital/${country}`,
-                success: function (data) {
-                    data.forEach((country) => {
-                            $("#country").html(`<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`);
-                    })
-                },
-            });
-        };
-    } else {
-        getAllCountries();
+  if (country) {
+    if (search === "country" || !search) {
+      $.ajax({
+        url: `https://restcountries.com/v3.1/name/${country}`,
+        success: function (data) {
+          data.forEach((country) => {
+            $("#country").html(
+              `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
+            );
+          });
+        },
+      });
+    } else if (search === "capital") {
+      $.ajax({
+        url: `https://restcountries.com/v3.1/capital/${country}`,
+        success: function (data) {
+          data.forEach((country) => {
+            $("#country").html(
+              `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
+            );
+          });
+        },
+      });
     }
+  } else {
+    getAllCountries();
+  }
+});
+
+$("select").change(function () {
+  select = $("select option:selected").val();
+
+  console.log(select);
+  $.ajax({
+    url: `https://restcountries.com/v3.1/region/${select}`,
+    success: function (data) {
+      data.forEach((country) => {
+        console.log(select);
+        $("#country").append(
+          `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
+        );
+      });
+    },
+  });
 });
 
 // API REQUEST + LIST ALL
 
 function getAllCountries() {
-    $.ajax({
-        url: "https://restcountries.com/v3.1/all",
-        success: function (data) {
-            data.forEach((country) => {
-                $("#country").append(`<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`)
-            })
-        }
-    })
+  $.ajax({
+    url: "https://restcountries.com/v3.1/all",
+    success: function (data) {
+      data.forEach((country) => {
+        $("#country").append(
+          `<li> Country: ${country.name.common} - Capital: ${country.capital} </li>`
+        );
+      });
+    },
+  });
 }
 
 getAllCountries();
